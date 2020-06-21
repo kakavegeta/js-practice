c30ddf0aeacc1c6e92b9959df632c96d8ccc067a
vegeka <zhj199305@gmail.com>
2020-06-21 05:47:39 -0400

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(function (req, res, next) {
+  console.log('1')
+  next(new Error('haha'))
+})
+
+app.use(function (req, res, next) {
+  console.log('2')
+  res.status(200).end()
+})
+
+app.listen(3000)
